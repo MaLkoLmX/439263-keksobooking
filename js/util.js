1d661695daf3a4265a4a181d12aa44add61df8db
MaLkoLmX <kadirovfamily@rambler.ru>
2017-12-13 13:43:55 +0400

    Исправление

--- js/util.js
+++ js/util.js
@@ -24,28 +24,4 @@ window.util = (function () {
     }
   };
 })();
-// (function () {
-//   window.getRandom = function (rand) {
-//     return Math.floor(Math.random() * rand.length);
-//   };
-// })();
 
-// (function () {
-//   window.getRandomNumber = function (min, max) {
-//     return Math.floor(Math.random() * (max - min) + min);
-//   };
-// })();
-
-// (function () {
-//   window.invalidFields = function (field) {
-//     field.style.borderWidth = '5px';
-//     field.style.borderColor = 'red';
-//   };
-// })();
-
-// (function () {
-//   window.removeBorder = function (field) {
-//     field.style.borderWidth = '';
-//     field.style.borderColor = '';
-//   };
-// })();
