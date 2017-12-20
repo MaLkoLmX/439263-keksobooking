77a1e6e208f0fe654891ce11e4de33471cad9c93
MaLkoLmX <kadirovfamily@rambler.ru>
2017-12-20 14:23:54 +0400

    Исправления

--- js/card.js
+++ js/card.js
@@ -11,11 +11,16 @@
     var cardTemplate = document.querySelector('template').content.querySelector('.map__card'); // шаблон карты
     var cardElement = cardTemplate.cloneNode(true);
     var cardElementP = cardElement.querySelectorAll('p');
-    var list = cardElement.querySelector('.popup__features');
+    var listFeatures = cardElement.querySelector('.popup__features');
+    var listPictures = cardElement.querySelector('.popup__pictures');
 
     function getFeatures(feature) {
       return '<li class="feature feature--' + feature + '"></li>';
     }
+
+    var getPictures = function (picture) {
+      return '<li><img src="' + picture + '"; width=50; height=50></li>';
+    };
 
     cardElement.querySelector('h3').textContent = ad.offer.title;
     cardElement.querySelector('small').textContent = ad.offer.address;
@@ -23,9 +28,12 @@
     cardElement.querySelector('h4').textContent = getType[ad.offer.type];
     cardElementP[2].textContent = ad.offer.rooms + ' для ' + ad.offer.guests + ' гостей.';
     cardElementP[3].textContent = 'Заезд после ' + ad.offer.checkin + ', выезд до ' + ad.offer.checkout;
-    list.innerHTML = '';
-    list.insertAdjacentHTML('afterBegin', ad.offer.features.map(getFeatures).join(''));
-    cardElement.appendChild(list);
+    listFeatures.innerHTML = '';
+    listFeatures.insertAdjacentHTML('afterBegin', ad.offer.features.map(getFeatures).join(''));
+    cardElement.appendChild(listFeatures);
+    listPictures.innerHTML = '';
+    listPictures.insertAdjacentHTML('afterBegin', ad.offer.photos.map(getPictures).join(''));
+    cardElement.appendChild(listPictures);
     cardElementP[4].textContent = ad.offer.description;
     cardElement.querySelector('img').src = ad.author.avatar;
     return cardElement;
