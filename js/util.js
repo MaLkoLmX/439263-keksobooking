f48c017b705d9b492ca9f4e4345081ac8f31f3fb
MaLkoLmX <kadirovfamily@rambler.ru>
2017-12-26 22:55:12 +0400

    Исправления

--- js/util.js
+++ js/util.js
@@ -13,12 +13,8 @@
   function errorHandler(error) {
     var node = document.createElement('div');
     node.style = 'position: fixed; top: 400px; left: 50%; z-index: 100; fontSize: 30px; margin: 0 auto; margin-left: -200px; padding: 20px; text-align: center; background-color: black; color: red; border: 2px solid red; border-radius: 50%; width: 400px';
-    // node.style.position = 'fixed';
-    // node.style.top = '400px';
-    // node.style.left = '50%';
-    // node.style.fontSize = '30px';
-
     node.textContent = error;
+
     document.body.insertAdjacentElement('afterbegin', node);
   }
 
